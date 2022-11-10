38c98b6e6c86f0a13e31e3572382c2eb3aa363d6
Dinesh Chhantyal <69712191+dineshchhantyal@users.noreply.github.com>
2022-11-10 16:51:29 -0600

    drawn interval

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 from matplotlib.patches import PathPatch
@@ -8,7 +7,8 @@ LOWER_BOUND = 0
 UPPER_BOUND = 100
 
 interval_x = []
-    
+fig, ax = plt.subplots()
+
 def draw_main_graph():
 
     N = 900
@@ -16,21 +16,14 @@ def draw_main_graph():
     x, y = x, -x**2
 
 
-    plt.plot(x, y, label = "Main plot")
-
-
-
-
-
+    ax.plot(x, y, label = "Main plot")
 
 def draw_interval(n = 4):
- 
     interval_x = np.linspace(LOWER_BOUND, UPPER_BOUND, n)
     for sub_interval in interval_x:
-    
-        plt.plot(sub_interval, - sub_interval ** 2, label = "subinterval")
+        plt.axvline(x = sub_interval, color='lightgrey', linestyle='--' )
+        plt.axhline(y=- sub_interval ** 2, color='lightgrey', linestyle='--')
     
 draw_main_graph()
 draw_interval()
-plt.ion()
 plt.show()
